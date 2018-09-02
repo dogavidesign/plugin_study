92a2fe39b84b1f2b1c01e3299b2abd38169b1079
dogavidesign <42906188+dogavidesign@users.noreply.github.com>
2018-09-02 19:08:15 +0900

    Add files via upload

---
+++ swiper/js/script.js
@@ -0,0 +1,16 @@
+
+
+    var mySwiper = new Swiper ('.swiper-container', {
+    loop: true,
+
+    // 페이지 표시 버튼
+    pagination: {
+      el: '.swiper-pagination',
+    },
+
+    // 이전, 다음 넘김 버튼
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    }
+  })
